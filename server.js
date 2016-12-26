5cf3cd621d5580e1391328379d896b638290a3b8
wenfeng <531075870@qq.com>
2016-12-26 10:59:42 +0800

    更新2.0

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ var proxy=require('http-proxy-middleware')
 //实例express
 var app = express();
 
-app.use('/api',proxy({
-	target:'http://guanjp.com:9805',
-	changeOrigin:true,
-	ws:true,
-	cookieRewrite:true,
-	pathRewrite:{
-		'^/api':'/'
-	}
-}));
+//app.use('/api',proxy({
+//	target:'http://guanjp.com:9805',
+//	changeOrigin:true,
+//	ws:true,
+//	cookieRewrite:true,
+//	pathRewrite:{
+//		'^/api':'/'
+//	}
+//}));
 
 console.log(__dirname);
 //拼接物理路径，用来指定虚拟路径的访问（静态页面文件）
@@ -34,29 +34,29 @@ app.listen(16919,function(){
 })
 
 
-var sha1 = require("sha1");
-    //定义验证接口
-    app.use('/wexin', function(req, res){
-        //获取get传递数据
-        var obj = req.query;
-        console.log("weixin", obj);
-        //将数据添加到一个数组
-        var arr = ["testtask", obj.timestamp, obj.nonce];
-        //排序
-        arr.sort();
-
-        //拼接字符串，并进行 sha1 加密
-        var str = sha1(arr.join(""));
-        console.log('sha1   ', str);
-
-        console.log('signature', obj.signature === str)
-        //匹配是否是微信请求
-        if( obj.signature === str){
-            //成功返回 echostr 随机字符串
-            res.send(obj.echostr).end();
-        }else{
-            res.send("验证失败").end();
-        }
-    });
+//var sha1 = require("sha1");
+//  //定义验证接口
+//  app.use('/wexin', function(req, res){
+//      //获取get传递数据
+//      var obj = req.query;
+//      console.log("weixin", obj);
+//      //将数据添加到一个数组
+//      var arr = ["testtask", obj.timestamp, obj.nonce];
+//      //排序
+//      arr.sort();
+//
+//      //拼接字符串，并进行 sha1 加密
+//      var str = sha1(arr.join(""));
+//      console.log('sha1   ', str);
+//
+//      console.log('signature', obj.signature === str)
+//      //匹配是否是微信请求
+//      if( obj.signature === str){
+//          //成功返回 echostr 随机字符串
+//          res.send(obj.echostr).end();
+//      }else{
+//          res.send("验证失败").end();
+//      }
+//  });
 
 module.exports = app;
